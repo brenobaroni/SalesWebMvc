ddf29e42e1fcdce8f04e962125c2062c384ab2d0
Breno Baroni <breno_baroni@hotmail.com>
2019-11-15 21:09:42 -0300

    Other Entities and finalized migration num 2#

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,29 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SalesStatus SaleStatus { get; set; }
+        public Seller Seller { get; set; } //#Every seller has a registered sale. BB
+
+        public SalesRecord()
+        {
+            //Empty Constructor
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SalesStatus saleStatus, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            SaleStatus = saleStatus;
+            Seller = seller;
+        }
+    }
+}
